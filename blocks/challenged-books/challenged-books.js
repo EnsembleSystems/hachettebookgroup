32fc8bf16506a44044eb309de5a3be12720fb706
Don Cosio <donc@ensemble.com>
2025-06-16 14:54:33 -0400

    complete

--- blocks/challenged-books/challenged-books.js
+++ blocks/challenged-books/challenged-books.js
@@ -7,4 +7,23 @@ export default function decorate(block) {
 
     gridContainer.insertBefore(separatorDiv, gridContainer.children[1]);
   }
+
+  const mediaQuery = window.matchMedia('(max-width: 1023px)');
+  let mobileElement = null;
+
+  const handleViewportChange = (e) => {
+    if (e.matches) {
+      if (!mobileElement) {
+        mobileElement = document.createElement('div');
+        mobileElement.classList.add('mobile');
+        block.appendChild(mobileElement);
+      }
+    } else {
+      mobileElement?.remove();
+      mobileElement = null;
+    }
+  };
+
+  handleViewportChange(mediaQuery);
+  mediaQuery.addEventListener('change', handleViewportChange);
 }
